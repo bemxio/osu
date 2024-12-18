7f14d9a3efeb542e38ab4d15bc60b0cd2666a1fb
bemxio <bemxiov@protonmail.com>
2024-12-18 18:26:18 +0100

    implement functionality for the Human Relax mod

--- osu.Game.Rulesets.Osu/Mods/OsuModHumanRelax.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModHumanRelax.cs
@@ -2,14 +2,21 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
 using osu.Game.Graphics;
 using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Osu.Objects;
+using osu.Game.Rulesets.Osu.Replays;
+using osu.Game.Rulesets.Osu.UI;
+using osu.Game.Rulesets.UI;
+using static osu.Game.Input.Handlers.ReplayInputHandler;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
-    public class OsuModHumanRelax : Mod
+    public class OsuModHumanRelax : Mod, IApplicableToDrawableRuleset<OsuHitObject>, IUpdatableByPlayfield
     {
         public override string Name => "Human Relax";
         public override string Acronym => "HRX";
@@ -27,5 +34,34 @@ namespace osu.Game.Rulesets.Osu.Mods
             typeof(OsuModAlternate),
             typeof(OsuModSingleTap)
         };
+
+        private OsuInputManager inputManager = null!;
+        private List<OsuReplayFrame> replayFrames = null!;
+        private int currentFrameIndex = 0;
+
+        public void ApplyToDrawableRuleset(DrawableRuleset<OsuHitObject> drawableRuleset)
+        {
+            inputManager = ((DrawableOsuRuleset)drawableRuleset).KeyBindingInputManager;
+            replayFrames = new OsuAutoGenerator(drawableRuleset.Beatmap, drawableRuleset.Mods).Generate().Frames.Cast<OsuReplayFrame>().ToList();
+        }
+        
+        public void Update(Playfield playfield)
+        {
+            if (currentFrameIndex == replayFrames.Count - 1)
+            {
+                return;
+            }
+
+            OsuReplayFrame currentFrame = replayFrames[currentFrameIndex];
+            OsuReplayFrame nextFrame = replayFrames[currentFrameIndex + 1];
+
+            double time = playfield.Clock.CurrentTime;
+
+            if (Math.Abs(nextFrame.Time - time) <= Math.Abs(currentFrame.Time - time))
+            {
+                new ReplayState<OsuAction> { PressedActions = currentFrame.Actions }.Apply(inputManager.CurrentState, inputManager);
+                currentFrameIndex++;
+            }
+        }
     }
 }
