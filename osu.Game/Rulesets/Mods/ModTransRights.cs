a1c394408ff9ffe2e896499cdf65ca37761dc6cc
bemxio <bemxiov@protonmail.com>
2024-12-18 19:11:22 +0100

    add a new TRANS RIGHTS mod

---
+++ osu.Game/Rulesets/Mods/ModTransRights.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Localisation;
+
+namespace osu.Game.Rulesets.Mods
+{
+    public sealed class ModTransRights : Mod
+    {
+        public override string Name => "TRANS RIGHTS";
+        public override string Acronym => "LGBTQIA+";
+        public override LocalisableString Description => "TRANS RIGHTS ARE HUMAN RIGHTS";
+        public override double ScoreMultiplier => 1;
+        public override ModType Type => ModType.Fun;
+    }
+}
