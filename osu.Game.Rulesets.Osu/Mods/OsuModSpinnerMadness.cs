075569b86ee30c830fc5b5c4561fc38e81855a35
bemxio <bemxiov@protonmail.com>
2025-12-17 20:25:35 +0100

    add various modes to SPINNER-MADNESS mod

--- osu.Game.Rulesets.Osu/Mods/OsuModSpinnerMadness.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModSpinnerMadness.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Bindables;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
+using osu.Game.Configuration;
 using osu.Game.Graphics;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Mods;
@@ -22,12 +24,42 @@ namespace osu.Game.Rulesets.Osu.Mods
         public override IconUsage? Icon => OsuIcon.ModSpunOut;
         public override ModType Type => ModType.Bemmy;
 
+        [SettingSource("Spinner replacement mode", "The way in which hit circles and sliders are replaced with spinners.")]
+        public Bindable<ReplacementMode> SpinnerReplacementMode { get; } = new Bindable<ReplacementMode>();
+
         public void ApplyToBeatmap(IBeatmap beatmap)
         {
             if (beatmap is not OsuBeatmap osuBeatmap)
                 return;
 
             List<OsuHitObject> newHitObjects = new List<OsuHitObject>();
+
+            if (SpinnerReplacementMode.Value == ReplacementMode.Classic)
+            {
+                Spinner endlessSpinner = new Spinner
+                {
+                    Position = OsuPlayfield.BASE_SIZE / 2,
+                    StartTime = osuBeatmap.HitObjects[0].StartTime,
+                };
+
+                switch (osuBeatmap.HitObjects[^1])
+                {
+                    case HitCircle circle:
+                        endlessSpinner.EndTime = circle.StartTime; break;
+
+                    case Slider slider:
+                        endlessSpinner.EndTime = slider.EndTime; break;
+
+                    case Spinner spinner:
+                        endlessSpinner.EndTime = spinner.EndTime; break;
+                }
+
+                endlessSpinner.ApplyDefaults(osuBeatmap.ControlPointInfo, osuBeatmap.Difficulty);
+                newHitObjects.Add(endlessSpinner);
+                osuBeatmap.HitObjects = newHitObjects;
+
+                return;
+            }
 
             foreach (OsuHitObject hitObject in osuBeatmap.HitObjects)
             {
@@ -36,7 +68,7 @@ namespace osu.Game.Rulesets.Osu.Mods
                     case HitCircle circle:
                         Spinner hitCircleSpinner = new Spinner
                         {
-                            Position = OsuPlayfield.BASE_SIZE / 2,
+                            Position = SpinnerReplacementMode.Value == ReplacementMode.Silly ? circle.Position : OsuPlayfield.BASE_SIZE / 2,
                             StartTime = circle.StartTime,
                             EndTime = circle.StartTime,
                             Samples = circle.Samples,
@@ -54,7 +86,7 @@ namespace osu.Game.Rulesets.Osu.Mods
                     case Slider slider:
                         Spinner sliderSpinner = new Spinner
                         {
-                            Position = OsuPlayfield.BASE_SIZE / 2,
+                            Position = SpinnerReplacementMode.Value == ReplacementMode.Silly ? slider.Position : OsuPlayfield.BASE_SIZE / 2,
                             StartTime = slider.StartTime,
                             EndTime = slider.EndTime,
                             Samples = slider.Samples,
@@ -75,6 +107,13 @@ namespace osu.Game.Rulesets.Osu.Mods
 
                 osuBeatmap.HitObjects = newHitObjects;
             }
+        }
+
+        public enum ReplacementMode
+        {
+            Standard,
+            Classic,
+            Silly
         }
     }
 }
